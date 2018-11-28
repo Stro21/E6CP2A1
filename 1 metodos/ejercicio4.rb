9929ba0e9c427f350fe00682b1b7149e9a618b60
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 12:51:04 -0300

    Se completó el ejercicio 4 de los métodos.

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,12 @@
+# rubocop:disable AsciiComments
 # Crear un método que muestre en pantalla un saludo, el método
 # debe recibir un parámetro, si ese parámetro es el string "Hola" el
 # método debe mostrar en pantalla "Hola Mundo".
+
+def saludo(hello)
+  puts 'Hola Mundo.' if hello == 'Hola'
+end
+
+saludo('Hola')
+
+# rubocop:enable AsciiComments

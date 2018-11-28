1caeb5483bcbe93333d9863311a48b2719955308
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 15:21:55 -0300

    Se completó el ejercicio 5 de los métodos.

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,13 @@
+# rubocop:disable AsciiComments
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def numeros_positivos(num1, num2)
+  for i in num1+1..num2-1 do
+    puts i.to_s if (i % 2).zero?
+  end
+end
+
+numeros_positivos(3, 14)
+
+# rubocop:enable AsciiComments

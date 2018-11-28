342da7a42a2b2ef28a51ad2733b90ee180c9f7f0
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 19:22:24 -0300

    Avanzando en el ejercicio 2 de los strings.

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -1,4 +1,9 @@
+# rubocop:disable AsciiComments
+# rubocop:disable LeadingCommentSpace
 # Construir un arreglo de los nombres de todos sus compañeros y en base a él:
 # 1. Imprimir todos los elementos que excedan más de 5 caracteres.
 # 2. Crear un arreglo nuevo con todos los elementos en minúscula.
-# 3. Crear un método que devuelva un arreglo con la cantidad de caracteres que tiene cada nombre.
+# 3. Crear un método que devuelva un arreglo con la cantidad de caracteres
+# que tiene cada nombre.
+# rubocop:enable AsciiComments
+# rubocop:enable LeadingCommentSpace

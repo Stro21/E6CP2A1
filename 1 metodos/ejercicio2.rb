1d1fb02cd0c5fc5339eb1e4800353cccaa77cb6a
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-27 23:13:59 -0300

    Se completo el ejercicio 2 de los métodos.

--- 1 metodos/ejercicio2.rb
+++ 1 metodos/ejercicio2.rb
@@ -1,9 +1,12 @@
+# rubocop:disable AsciiComments
 # El siguiente programa debería mostrar sí o no, sin embargo muestrar error
 # Se pide identificar el error y corregirlo.
+# rubocop:enable AsciiComments
 
 def random
   result = [true, false].sample
   puts result
+  result
 end
 
 if random == true

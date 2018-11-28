183b2c58c83842a56a343e2647e8632b930d4320
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 17:50:59 -0300

    Se completó el ejercicio 2 de los arrays.

--- 2 arrays/ejercicio2.rb
+++ 2 arrays/ejercicio2.rb
@@ -11,16 +11,41 @@
 #    invertir el orden de los elementos en un arreglo.
 # rubocop:enable AsciiComments
 
+def eliminar(array)
+  array.pop if array[array.length - 1] != 0
+  array
+end
+
+def invertir_array(array1, array2)
+  largo = array1.length
+  largo.times do
+    array2.push(array1[array1.length - 1])
+    array1.pop
+  end
+  array2
+end
+
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
 puts 'Arreglo no modificado.'
 print a
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
-puts "\nEliminar el último elemento."
+puts "\n1. Eliminar el último elemento."
 a.pop
 print a
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
-puts "\nEliminar el primer elemento."
+puts "\n2. Eliminar el primer elemento."
 a.shift
 print a
-puts "\nEliminar el elemento del medio"
+puts "\n3. Eliminar el elemento del medio"
 a.slice!((a.length / 2) - 1)
+print a
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+puts "\n4. Eliminar el último elemento que sea distinto a 1"
+a = eliminar(a)
+print a
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+b = []
+puts "\n5. Invertir array."
+b = invertir_array(a, b)
+print b
+puts ''

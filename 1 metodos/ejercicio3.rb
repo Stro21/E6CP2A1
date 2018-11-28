7f5e833a3a43473c85ee1a3fdda3860941e71cad
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-27 23:31:21 -0300

    Se completo el ejercicio 3 de los métodos.

--- 1 metodos/ejercicio3.rb
+++ 1 metodos/ejercicio3.rb
@@ -1,5 +1,18 @@
+# rubocop:disable AsciiComments
 # Escriba un método llamado check5 que devuelva true cuando
 # se le pase un número mayor a 5 y false en caso contrario.
 
-puts check5(5) # Debería ser false
-puts check5(6) # Debería ser true
+def check5(num)
+  num > 5
+end
+
+for i in 0..1 do
+  if check5(5 + i)
+    puts 'Es verdadera'
+  else
+    puts 'Es falsa'
+  end
+end
+# puts check5(5) Debería ser false
+# puts check5(6) Debería ser true
+# rubocop:enable AsciiComments

5bd37d09fafa1dcbf77eb9a2c6518436195bf887
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 19:15:09 -0300

    Se empezó a hacer el ejercicio 1 de los strings.

--- 3 strings/ejercicio1.rb
+++ 3 strings/ejercicio1.rb
@@ -1,7 +1,15 @@
-# Dado el siguiente string y caracter, crear un método que reciba como parámetro el string
-# y el caracter. Luego debe buscar si existe ese caracter dentro del string.
+# rubocop:disable AsciiComments
+# Dado el siguiente string y caracter, crear un método que reciba como parámetro
+# el string y el caracter.
+# Luego debe buscar si existe ese caracter dentro del string.
 # hint: El método .include? de un string busca si un caracter
 # o string dado está contenido en éste.
+# rubocop:enable AsciiComments
+
+def esta_dentro(str1, str2)
+
+end
 
 cadena = 'Hola Mundo!'
 caracter = 'o'
+esta_dentro(cadena, caracter)

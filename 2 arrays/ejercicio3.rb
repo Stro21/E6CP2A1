ac8365184b410c248bd2a160b5cb33590d5e6860
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 17:57:35 -0300

    Se empezó a hacer el ejercicio 4 de los array.

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -1,7 +1,18 @@
- # Dado el array:
- # 1. Crear un método para eliminar todos los números pares del arreglo.
- # 2. Crear un método para obtener la suma de todos los elementos del arreglo Utilizando .each
- # 3. Crear un método para obtener el promedio de un arreglo.
- # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
+# rubocop:disable AsciiComments
+# Dado el array:
+# 1. Crear un método para eliminar todos los números pares del arreglo.
+# 2. Crear un método para obtener la suma de todos los elementos del arreglo
+# Utilizando .each
+# 3. Crear un método para obtener el promedio de un arreglo.
+# 4. Crear un método que incrementa todos los elementos en una unidad y devuelva
+# un arreglo nuevo.
+# rubocop:enable AsciiComments
 
-a = [1,2,3,9,1,4,5,2,3,6,6]
+def eliminar_pares(array)
+
+end
+
+puts 'Arreglo sin modificar.'
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+print a
+print "\n"

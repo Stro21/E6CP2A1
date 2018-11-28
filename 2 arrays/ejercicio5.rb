fae619ab0cf7d1407225d8a478eb1fee016273f6
Eduardo Straub <eduardo.straub1@gmail.com>
2018-11-28 19:04:20 -0300

    Se completarón todos los ejercicios de los arrays

--- 2 arrays/ejercicio5.rb
+++ 2 arrays/ejercicio5.rb
@@ -1,16 +1,24 @@
+# rubocop:disable AsciiComments
+# rubocop:disable LeadingCommentSpace
 # Se tiene un arreglo de productos y precios, se pide que el output sea:
 # <div class='product'><p> Producto1 </p><p> Precio: 1000 </p></div>
 # <div class='product'><p> Producto2 </p><p> Precio: 2000 </p></div>
-# <div class='product'><p> Producto3 </p><p> Precio: 1500 </p></div>
+# <div class='product'><p> Producto3 </p><p> Precio: 1500 </p></div>
 # <div class='product'><p> Producto4 </p><p> Precio: 950 </p></div>
+# rubocop:enable AsciiComments
+# rubocop:enable LeadingCommentSpace
 
-products = %w(Producto1 Producto2 Producto3 Producto4)
+products = %w[Producto1 Producto2 Producto3 Producto4]
 prices = %w[1000 2000 1500 950]
-
+i = 0
 html = ''
-products.each do |i|
-  html += "<div class='product'>"
-  html += "</div>\n"
+products.each do
+  html += "<div class='product'><p> "
+  html += products[i]
+  html += ' </p><p> '
+  html += prices[i]
+  html += " </p></div>\n"
+  i += 1
 end
 
 puts html
